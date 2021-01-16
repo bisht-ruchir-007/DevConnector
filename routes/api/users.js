aef9684525385a585aad3b6721f995d013a1818f
Ruchir Bisht <ruchircr7@gmail.com>
2021-01-16 20:56:21 +0530

    User register completed

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const gravatar = require('gravatar');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
+
 const User = require('../../models/User');
 
 // @route       POST api/users
@@ -54,7 +57,18 @@ router.post(
 			await user.save();
 			//Return the jsonwebtoken
 
-			res.send('User Registered');
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000 }, (err, token) => {
+				if (err) {
+					throw err;
+				}
+				res.json({ token });
+			});
 		} catch (err) {
 			console.log(err.message);
 			res.status(500).send('Server Error');
