4474d44d225a7d9e1e643a509863f02ef64a1b77
Ruchir Bisht <ruchircr7@gmail.com>
2021-01-21 19:38:53 +0530

    Get Auth user middleware added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 // @route       GET api/auth
 // @description Test route
 // @access      Public
-router.get('/', (req, res) => {
-	res.send('Auth Router');
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send('Server error');
+	}
 });
 
 module.exports = router;
