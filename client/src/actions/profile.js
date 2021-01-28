772d85d859932c0260a27c8b20475bbb7b44fadd
Ruchir Bisht <ruchircr7@gmail.com>
2021-01-28 14:06:46 +0530

    Create Profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 import { PROFILE_ERROR, GET_PROFILE } from './types';
+import { setAlert } from './alert';
 
 // Get the current user profile
 export const getCurrentUserProfile = () => async (dispatch) => {
@@ -12,6 +13,41 @@ export const getCurrentUserProfile = () => async (dispatch) => {
 			payload: res.data
 		});
 	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+//Create or update Profile
+export const createProfile = (formData, history, edit = false) => async (dispatch) => {
+	try {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+		const res = await axios.post('/api/profile', formData, config);
+
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data
+		});
+
+		dispatch(setAlert(edit ? 'Profile Updated ' : 'Profile Created', 'success'));
+
+		// if creating profile
+		if (!edit) {
+			history.push('/dashboard');
+		}
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+		}
+
 		dispatch({
 			type: PROFILE_ERROR,
 			payload: { msg: err.response.statusText, status: err.response.status }
