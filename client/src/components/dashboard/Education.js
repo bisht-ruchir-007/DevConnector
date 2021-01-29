00a8b0ef7ed5c782a76590097734ed3552c587f0
Ruchir Bisht <ruchircr7@gmail.com>
2021-01-29 09:38:40 +0530

    modifing the user profile

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
 
 const Education = ({ education }) => {
 	const educations = education.map((edu) => (
@@ -18,23 +19,31 @@ const Education = ({ education }) => {
 			</td>
 		</tr>
 	));
-	return (
-		<Fragment>
-			<h2 className='my-2'>Education Credentials</h2>
-			<table className='table'>
-				<thead>
-					<tr>
-						<th>School or College</th>
-						<th className='hide-sm'>Degree</th>
-						<th className='hide-sm'>Field of Study or Branch</th>
-						<th className='hide-sm'>Years</th>
-						<th />
-					</tr>
-				</thead>
-				<tbody>{educations}</tbody>
-			</table>
-		</Fragment>
-	);
+	if (education.length > 0) {
+		return (
+			<Fragment>
+				<h2 className='my-2'>Education Credentials</h2>
+				<table className='table'>
+					<thead>
+						<tr>
+							<th>School or College</th>
+							<th className='hide-sm'>Degree</th>
+							<th className='hide-sm'>Field of Study or Branch</th>
+							<th className='hide-sm'>Years</th>
+							<th />
+						</tr>
+					</thead>
+					<tbody>{educations}</tbody>
+				</table>
+			</Fragment>
+		);
+	} else {
+		return (
+			<h3>
+				Its Empty , Please <Link to='/add-education'>add</Link> your experience details.
+			</h3>
+		);
+	}
 };
 
 Education.propTypes = {
--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
+import { Link } from 'react-router-dom';
 
 const Experience = ({ experience }) => {
 	const experiences = experience.map((exp) => (
@@ -17,22 +18,34 @@ const Experience = ({ experience }) => {
 			</td>
 		</tr>
 	));
-	return (
-		<Fragment>
-			<h2 className='my-2'>Experience Credentials</h2>
-			<table className='table'>
-				<thead>
-					<tr>
-						<th>Company</th>
-						<th className='hide-sm'>Title</th>
-						<th className='hide-sm'>Years (From - To)</th>
-						<th />
-					</tr>
-				</thead>
-				<tbody>{experiences}</tbody>
-			</table>
-		</Fragment>
-	);
+
+	if (experience.length > 0) {
+		return (
+			<Fragment>
+				<h2 className='my-2'>Experience Credentials</h2>
+				<table className='table'>
+					<thead>
+						<tr>
+							<th>Company</th>
+							<th className='hide-sm'>Title</th>
+							<th className='hide-sm'>Years (From - To)</th>
+							<th />
+						</tr>
+					</thead>
+					<tbody>{experiences}</tbody>
+				</table>
+			</Fragment>
+		);
+	} else {
+		return (
+			<Fragment>
+				<h2 className='my-2'>Education Credentials</h2>
+				<h3>
+					Its Empty , Please <Link to='/add-education'>add</Link> your experience details.
+				</h3>
+			</Fragment>
+		);
+	}
 };
 
 Experience.propTypes = {
