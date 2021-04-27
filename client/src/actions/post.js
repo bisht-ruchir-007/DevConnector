f34c26752299279849d80c4f664b93f2167b39b8
Ruchir Bisht <ruchircr7@gmail.com>
2021-04-27 13:17:39 +0530

    Delete post impletmented

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, UPDATE_LIKES, POST_ERROR } from './types';
+import { GET_POSTS, UPDATE_LIKES, DELETE_POST, POST_ERROR } from './types';
 
 //get posts
 export const getPosts = () => async (dispatch) => {
@@ -42,6 +42,23 @@ export const removeLike = (postId) => async (dispatch) => {
 			type: UPDATE_LIKES,
 			payload: { postId, likes: res.data }
 		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		});
+	}
+};
+
+// Delete post
+export const deletePost = (postId) => async (dispatch) => {
+	try {
+		const res = await axios.delete(`/api/posts/${postId}`);
+		dispatch({
+			type: DELETE_POST,
+			payload: postId
+		});
+		dispatch(setAlert('Post Removed', 'success'));
 	} catch (err) {
 		dispatch({
 			type: POST_ERROR,
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,4 @@
-import { GET_POSTS, UPDATE_LIKES, POST_ERROR } from '../actions/types';
+import { GET_POSTS, UPDATE_LIKES, DELETE_POST, POST_ERROR } from '../actions/types';
 
 const initialState = {
 	posts: [],
@@ -23,6 +23,12 @@ export default function(state = initialState, action) {
 				posts: state.posts.map(
 					(post) => (post._id === payload.postId ? { ...post, likes: payload.likes } : post)
 				),
+				loading: false
+			};
+		case DELETE_POST:
+			return {
+				...state,
+				posts: state.posts.filter((post) => post._id !== payload),
 				loading: false
 			};
 		case POST_ERROR:
